89ffa92ad9fa8daf762be979b2b587b41967ba22
Ales1099 <30334886+Ales1099@users.noreply.github.com>
2018-10-12 11:33:15 +0700

    Update main.c

--- OS/pr1/main.c
+++ OS/pr1/main.c
@@ -8,7 +8,7 @@
 #define PD  		0x000000003fe00000
 #define PT  		0x00000000001ff000
 #define OFFSET		0x0000000000000fff
-#define PERPOSE		0xfffffffffffff000
+#define PERPOSE		0x000ffffffffff000
 #define P           0x0000000000000001
 
 #define fPML4E(number)((number)>>39)
@@ -26,79 +26,87 @@ int main(int argc, char ** argv)
 	uint64_t PTE;
 	uint64_t offset;
 
-	long int m,q,r;
-	FILE * fileInput = fopen("input.txt","r+");
+	long int m, q, r;
+	FILE * fileInput = fopen("input.txt", "r+");
 	long int position = ftell(fileInput);
-	int error = fscanf(fileInput,"%ld %ld %ld",&m,&q,&r);
-	if(error == EOF)
+	int error = fscanf(fileInput, "%ld %ld %ld", &m, &q, &r);
+	if (error == EOF)
 	{
 		fclose(fileInput);
 		return(-1);
 	}
-	
-	memory_prt_value = (long int**)malloc(m*sizeof(long int*));
 
-	for(long int index = 0; index < m; ++index)
+	memory_prt_value = (long int**)malloc(m * sizeof(long int*));
+
+	for (long int index = 0; index < m; ++index)
 	{
-		memory_prt_value[index] = (long int*)malloc(2*sizeof(long int));
-		error = fscanf(fileInput,"%ld %ld",&memory_prt_value[index][0],&memory_prt_value[index][1]);
-		if(error == EOF)
+		memory_prt_value[index] = (long int*)malloc(2 * sizeof(long int));
+		error = fscanf(fileInput, "%ld %ld", &memory_prt_value[index][0], &memory_prt_value[index][1]);
+		if (error == EOF)
 		{
 			fclose(fileInput);
 			return(-1);
 		}
 	}
 
-	uint64_t local_logical_adress;
+	uint64_t local_logical_adress = 0;
 	uint64_t check_p;
 	uint64_t local_offset = r;
-	uint64_t physical_adress;
-	long int ok = 0;
+	uint64_t physical_adress = 0;
+	int ok = 0;
 
-	for(long int i = 0; i<q; i++)
+	for (long int i = 0; i < q; i++)
 	{
-		error = fscanf(fileInput,"%ld\n",&test);
-		if(error == EOF)
+		error = fscanf(fileInput, "%ld\n", &local_logical_adress);
+		if (error == EOF)
 		{
 			fclose(fileInput);
 			return(-1);
 		}
 		else
 		{
-			offset = test & OFFSET;
-			PML4E = test & PML4;
-			PDPTE = test & PDPT;
-			PDE = test & PD;
-			PTE = test & PT;
-	
-			PML4E = fPML4E(PML4E);
-			PDPTE = fPDPTE(PDPTE);
-			PDE = fPDE(PDE);
-			PTE = fPTE(PTE);
+			offset = local_logical_adress & OFFSET;
+			PML4E  = local_logical_adress & PML4;
+			PDPTE  = local_logical_adress & PDPT;
+			PDE    = local_logical_adress & PD;
+			PTE    = local_logical_adress & PT;
 
-			if(ok == 0)
+			PML4E >>= 39;
+			PDPTE >>= 30;
+			PDE   >>= 21;
+			PTE   >>= 12;
+
+			//PML4E = fPML4E(PML4E);
+			//PDPTE = fPDPTE(PDPTE);
+			//PDE = fPDE(PDE);
+			//PTE = fPTE(PTE);
+
+			if (ok == 0)
 			{
 				physical_adress = PML4E * 8 + local_offset;
 
-				for(long int i = 0; i<m; i++)
+				for (long int j = 0; j < m; j++)
 				{
-					if(physical_adress == memory_prt_value[i][0])
+					if (physical_adress == memory_prt_value[j][0])
 					{
-						check_p = memory_prt_value[i][1] & P;
-						printf("%ld\n", check_p );
-						if(check_p == 1)
+						check_p = memory_prt_value[j][1] & P;
+						//printf("%ld\n", check_p);
+						if (check_p == 1)
 						{
-							local_offset = (memory_prt_value[i][1] & PERPOSE);//>>12;
+							ok = 0;
+							local_offset = (memory_prt_value[j][1] & PERPOSE);
 							break;
-						//	printf("%ld\n", physical_adress);
-						//	printf("%ld\n", local_offset);
 						}
 						else
 						{
-							ok = 1;
-							printf("fault\n");
+							ok++;
+							//printf("fault\n");
 							break;
 						}
+					}
+					else
+					{
+						ok++;
 					}
 				}
 			}
@@ -109,23 +117,32 @@ int main(int argc, char ** argv)
 				continue;
 			}
 
-			if(ok == 0)
+			if (ok == 0)
 			{
 				physical_adress = PDPTE * 8 + local_offset;
 
-				for(long int i = 0; i<m; i++)
+				for (long int j = 0; j < m; j++)
 				{
-					if(physical_adress == memory_prt_value[i][0])
+					if (physical_adress == memory_prt_value[j][0])
 					{
-						check_p = memory_prt_value[i][1] & P;
-						printf("%ld\n", check_p );
-						if(check_p == 1)
+						check_p = memory_prt_value[j][1] & P;
+						//printf("%ld\n", check_p);
+						if (check_p == 1)
 						{
-							local_offset = (memory_prt_value[i][1] & PERPOSE);//>>12;
+							ok = 0;
+							local_offset = (memory_prt_value[j][1] & PERPOSE);
 							break;
-						//	printf("%ld\n", physical_adress);
-						//	printf("%ld\n", local_offset);						
 						}
+						else
+						{
+							ok++;
+							//printf("fault\n");
+							break;
+						}
+					}
+					else
+					{
+						ok++;
 					}
 				}
 			}
@@ -136,29 +153,32 @@ int main(int argc, char ** argv)
 				continue;
 			}
 
-			if(ok == 0)
+			if (ok == 0)
 			{
 				physical_adress = PDE * 8 + local_offset;
 
-				for(long int i = 0; i<m; i++)
+				for (long int j = 0; j < m; j++)
 				{
-					if(local_logical_adress == memory_prt_value[i][0])
+					if (physical_adress == memory_prt_value[j][0])
 					{
-						check_p = memory_prt_value[i][1] & P;
-						printf("%ld\n", check_p );
-						if(check_p == 1)
+						check_p = memory_prt_value[j][1] & P;
+						//printf("%ld\n", check_p);
+						if (check_p == 1)
 						{
-							local_offset = (memory_prt_value[i][1] & PERPOSE);//>>12;
+							ok = 0;
+							local_offset = (memory_prt_value[j][1] & PERPOSE);
 							break;
-							//printf("%ld\n", physical_adress);
-							//printf("%ld\n", local_offset);						
 						}
 						else
 						{
-							ok = 1;
-							printf("fault\n");
+							ok++;
+							//printf("fault\n");
 							break;
 						}
+					}
+					else
+					{
+						ok++;
 					}
 				}
 			}
@@ -169,29 +189,33 @@ int main(int argc, char ** argv)
 				continue;
 			}
 
-			if(ok == 0)
+
+			if (ok == 0)
 			{
 				physical_adress = PTE * 8 + local_offset;
 
-				for(long int i = 0; i<m; i++)
+				for (long int j = 0; j < m; j++)
 				{
-					if(physical_adress == memory_prt_value[i][0])
+					if (physical_adress == memory_prt_value[j][0])
 					{
-						check_p = memory_prt_value[i][1] & P;
-						printf("%ld\n", check_p );
-						if(check_p == 1)
+						check_p = memory_prt_value[j][1] & P;
+						//printf("%ld\n", check_p);
+						if (check_p == 1)
 						{
-							local_offset = (memory_prt_value[i][1] & PERPOSE);
+							ok = 0;
+							local_offset = (memory_prt_value[j][1] & PERPOSE);
 							break;
-							//printf("%ld\n", physical_adress);
-							//printf("%ld\n", local_offset);
 						}
 						else
 						{
-							ok = 1;
-							printf("fault\n");
+							ok ++;
+							//printf("fault\n");
 							break;
 						}
+					}
+					else
+					{
+						ok++;
 					}
 				}
 			}
@@ -202,53 +226,32 @@ int main(int argc, char ** argv)
 				continue;
 			}
 
-			physical_adress = local_offset;
-			if(ok == 0)
-			{
-				for(long int i = 0; i<m; i++)
-				{
-					if(physical_adress == memory_prt_value[i][0])
-					{
-						check_p = memory_prt_value[i][1] & P;
-						printf("%ld\n", check_p);
-						if(check_p == 1)
-						{
-							local_offset = (memory_prt_value[i][1] & PERPOSE);
-							break;
-						}
-						else
-						{
-							ok = 1;
-							printf("fault\n");
-							break;
-						}
-					}
-				}
-			}
-			else
-			{
-				ok = 0;
-				printf("fault\n");
-				continue;
-			}
-
-			if(ok == 0)
+			if (ok == 0)
 			{
 				physical_adress = local_offset + offset;
 
 				printf("%ld \n", physical_adress);
 
 				local_offset = r;
+				PML4E = 0;
+				PDPTE = 0;
+				PDE = 0;
+				PTE = 0;
+				physical_adress = 0;
+				offset = 0;
+				ok = 0;
 			}
 			else
 			{
 				ok = 0;
 				printf("fault\n");
+				continue;
 			}
 		}
 
 	}
 
 	fclose(fileInput);
+	system("pause");
 	return(0);
 }
